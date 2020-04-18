8f88d421748c68813fa6ef9c6071a303565481cf
daryl <sai9375@hotmail.com>
2020-04-19 00:24:42 +0800

    commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,21 @@ const app = getApp()
 
 Page({
   data: {
-    currentTab: 1
+    currentTab: 1,
+    bangumis: []
   },
 
   switchTab(e) {
     this.setData({
-      currentTab: e.detail.current + 1
+      currentTab: e.detail.current + 1,
+      bangumis: [
+        {
+          "title": "神之塔",
+          "time": "19:00",
+          "copyright": "哔哩哔哩",
+          "avatar": "https://i0.hdslb.com/bfs/bangumi/image/b7ce6fd3aacb2edd1bd75e368e8838c2d768dab8.png"
+        }
+      ]
     })
   }
 })
