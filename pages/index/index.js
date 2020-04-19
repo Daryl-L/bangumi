dbea79b072b7722404d60cbdef0bcd626a37aec1
daryl <sai9375@hotmail.com>
2020-04-19 21:43:11 +0800

    commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,8 +9,12 @@ Page({
   },
 
   switchTab(e) {
+    var current = e.detail.current
+    if (current == 7) {
+      current = 0;
+    }
     this.setData({
-      currentTab: e.detail.current + 1,
+      currentTab: current + 1,
       bangumis: [
         {
           "title": "神之塔",
@@ -19,6 +23,26 @@ Page({
           "avatar": "https://i0.hdslb.com/bfs/bangumi/image/b7ce6fd3aacb2edd1bd75e368e8838c2d768dab8.png"
         }
       ]
+    })
+  },
+
+  onLoad: function (options) {
+    this.setData({
+      currentTab: 1,
+      bangumis: [
+        {
+          "title": "神之塔",
+          "time": "19:00",
+          "copyright": "哔哩哔哩",
+          "avatar": "https://i0.hdslb.com/bfs/bangumi/image/b7ce6fd3aacb2edd1bd75e368e8838c2d768dab8.png"
+        }
+      ]
+    })
+  },
+
+  requestForBangumis: function (time) {
+    wx.request({
+      url: 'url',
     })
   }
 })
